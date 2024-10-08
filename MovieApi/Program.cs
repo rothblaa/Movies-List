d3f78305eedda3b97311c1b5165fa0aefc4f89ae
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 03:36:31 -0500

    added use of CORS

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -9,7 +9,20 @@ namespace MovieApi
             //add services
             builder.Services.AddControllers();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("MyCors");
 
             //add mapping
             app.MapControllers();
