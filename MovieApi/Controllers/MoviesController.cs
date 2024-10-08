a0b823cc30ff74377475521d835d3784ab8c0fba
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-07 20:59:42 -0500

    Created movies controller and get all movies endpoint

--- MovieApi/Controllers/MoviesController.cs
+++ MovieApi/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
 
 namespace MovieApi.Controllers
 {
@@ -7,5 +9,18 @@ namespace MovieApi.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-    }
+        private Movie[] _movies = new Movie[]
+        {
+            new Movie { Id = 1, Title = "Title One", Director = "Director One", Rating = 1 },
+            new Movie { Id = 2, Title = "Title Two", Director = "Director Two", Rating = 2 },
+            new Movie { Id = 3, Title = "Title Three", Director = "Director Three", Rating = 3 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies() 
+        {
+            return Ok(_movies);
+        }
+
+    };
 }
