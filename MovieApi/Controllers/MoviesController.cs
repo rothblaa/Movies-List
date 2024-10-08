5a713bd8344289a9acbc1f84a4689332db5ca5fb
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-07 20:14:09 -0500

    Add project files.

---
+++ MovieApi/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.Controllers
+{
+    // https://localhost:7148/api/movies
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+    }
+}
---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Rating { get; set; }
+    }
+}
